53ece41356d1ccec22f57ffc092eb0eac190e803
Your Name <novemberox@gmail.com>
2011-09-23 14:07:24 +0200

    first commit

---
+++ src/pl/html5videofail/WideoActivity.java
@@ -0,0 +1,53 @@
+package pl.html5videofail;
+
+import pl.onet.video.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.FrameLayout;
+
+public class WideoActivity extends Activity {
+
+	private FrameLayout customComponenet;
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		((WebView) findViewById(R.id.webView)).saveState(outState);
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		customComponenet = (FrameLayout) findViewById(R.id.custom);
+
+		WebView webView = (WebView) findViewById(R.id.webView);
+		webView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setDomStorageEnabled(false);
+		webView.getSettings().setSupportMultipleWindows(false);
+		webView.getSettings().setAllowFileAccess(false);
+		webView.getSettings().setAppCacheEnabled(false);
+		webView.getSettings().setNeedInitialFocus(false);
+		webView.getSettings().setSupportZoom(false);
+
+		webView.setWebChromeClient(new WebChromeClient() {
+
+			@Override
+			public void onShowCustomView(View view, CustomViewCallback callback) {
+				customComponenet.addView(view);
+			}
+		});
+
+		if (savedInstanceState == null) {
+			webView.loadUrl("file:///android_asset/index.html");
+		} else {
+			webView.restoreState(savedInstanceState);
+		}
+	}
+}
